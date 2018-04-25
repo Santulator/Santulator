99cdcc0d556b9f2736243de1d65a9ff2c9038dde
Adam Carroll <AdamCarroll@users.noreply.github.com>
2018-04-25 22:46:57 +0200

    Added the test for the log tool

---
+++ gui/src/test/java/io/github/santulator/gui/main/ExecutableLogToolTest.java
@@ -0,0 +1,65 @@
+package io.github.santulator.gui.main;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import io.github.santulator.core.SantaException;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+
+import static java.util.stream.Collectors.joining;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExecutableLogToolTest {
+    private static final Logger TARGET_LOG = (Logger) LoggerFactory.getLogger(ExecutableLogTool.class);
+
+    private final LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+    private final AppenderForTesting appender = new AppenderForTesting();
+
+    @BeforeEach
+    public void setUp() {
+        TARGET_LOG.addAppender(appender);
+        TARGET_LOG.setLevel(Level.INFO);
+        TARGET_LOG.setAdditive(true);
+        appender.setContext(loggerContext);
+        appender.start();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        appender.stop();
+        TARGET_LOG.detachAppender(appender);
+    }
+
+    @Test
+    public void testLogSystemDetails() {
+        validate(ExecutableLogTool::logSystemDetails);
+    }
+
+    @Test
+    public void testLogShutdown() {
+        validate(ExecutableLogTool::logShutdown);
+    }
+
+    @Test
+    public void testLogStartup() {
+        validate(ExecutableLogTool::logStartup);
+    }
+
+    @Test
+    public void testLogError() {
+        validate(() -> ExecutableLogTool.logError(new SantaException("Test")));
+    }
+
+    private void validate(final Runnable r) {
+        r.run();
+
+        String messages = appender.getMessages().stream()
+            .collect(joining());
+        assertTrue(StringUtils.isNotBlank(messages));
+    }
+}
