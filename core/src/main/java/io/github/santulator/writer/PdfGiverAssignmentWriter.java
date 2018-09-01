efa9020eb2d24221d65e79a41714618e753aff6a
Adam Carroll <AdamCarroll@users.noreply.github.com>
2018-09-01 14:43:40 +0200

    Improved PDF that is generated

--- core/src/main/java/io/github/santulator/writer/PdfGiverAssignmentWriter.java
+++ core/src/main/java/io/github/santulator/writer/PdfGiverAssignmentWriter.java
@@ -8,6 +8,7 @@ import com.lowagie.text.*;
 import com.lowagie.text.Font;
 import com.lowagie.text.Image;
 import com.lowagie.text.pdf.PdfWriter;
+import io.github.santulator.core.CoreConstants;
 import io.github.santulator.core.SantaException;
 import io.github.santulator.model.GiverAssignment;
 import org.apache.commons.lang3.StringUtils;
@@ -15,7 +16,6 @@ import org.apache.commons.lang3.StringUtils;
 import java.awt.*;
 import java.io.OutputStream;
 import java.net.URL;
-import java.nio.charset.Charset;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
@@ -23,6 +23,14 @@ import javax.inject.Singleton;
 @Singleton
 public class PdfGiverAssignmentWriter implements GiverAssignmentWriter {
     public static final String FORMAT_SUFFIX = ".pdf";
+
+    private static final String FONT_NAME = "Times-Roman";
+
+    private static final int FONT_SIZE_MAIN = 16;
+
+    private static final int FONT_SIZE_GIFT_RECEIVER = 20;
+
+    private static final float IMAGE_SCALE_PERCENTAGE = 30;
 
     private final URL headerImage;
 
@@ -39,11 +47,11 @@ public class PdfGiverAssignmentWriter implements GiverAssignmentWriter {
     @Override
     public void writeGiverAssignment(final String name, final GiverAssignment assignment, final OutputStream out, final String password) {
         try {
-            Document document = new Document(PageSize.A5.rotate());
+            Document document = new Document(PageSize.A5);
             PdfWriter writer = PdfWriter.getInstance(document, out);
 
             if (StringUtils.isNotBlank(password)) {
-                byte[] bytes = password.getBytes(Charset.forName("UTF-8"));
+                byte[] bytes = password.getBytes(CoreConstants.CHARSET);
 
                 writer.setEncryption(bytes, null, PdfWriter.ALLOW_PRINTING, PdfWriter.STANDARD_ENCRYPTION_128);
             }
@@ -51,11 +59,14 @@ public class PdfGiverAssignmentWriter implements GiverAssignmentWriter {
 
             Image header = Image.getInstance(headerImage);
 
+            header.setAlignment(Element.ALIGN_CENTER);
+            header.scalePercent(IMAGE_SCALE_PERCENTAGE);
+
             document.open();
             document.add(header);
-            addParagraph(document, assignment.getFrom().getName(), 16, Font.BOLDITALIC, Color.BLACK);
-            addParagraph(document, phrase, 16, Font.ITALIC, Color.BLACK);
-            addParagraph(document, assignment.getTo().getName(), 20, Font.BOLDITALIC, Color.RED);
+            addParagraph(document, assignment.getFrom().getName(), FONT_SIZE_MAIN, Font.BOLDITALIC, Color.BLACK);
+            addParagraph(document, phrase, FONT_SIZE_MAIN, Font.ITALIC, Color.BLACK);
+            addParagraph(document, assignment.getTo().getName(), FONT_SIZE_GIFT_RECEIVER, Font.BOLDITALIC, Color.RED);
             document.close();
         } catch (Exception e) {
             throw new SantaException(String.format("Unable to create PDF '%s'", name), e);
@@ -63,7 +74,7 @@ public class PdfGiverAssignmentWriter implements GiverAssignmentWriter {
     }
 
     private void addParagraph(final Document document, final String text, final int size, final int style, final Color colour) throws DocumentException {
-        Font font = FontFactory.getFont("Times-Roman", size, style, colour);
+        Font font = FontFactory.getFont(FONT_NAME, size, style, colour);
         Paragraph paragraph = new Paragraph(text, font);
 
         paragraph.setAlignment(Element.ALIGN_CENTER);
