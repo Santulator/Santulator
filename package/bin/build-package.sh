a8a96a145bb47e1c05e8cdec2718f576daed87ce
Adam Carroll <AdamCarroll@users.noreply.github.com>
2018-10-28 12:38:58 +0100

    Added packaging for JFX 11

---
+++ package/bin/build-package.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+PACKAGER=$1
+INSTALLER_TYPE=$2
+MODULE_PATH=$3
+INPUT=$4
+OUTPUT=$5
+JAR=$6
+VERSION=$7
+
+${PACKAGER} \
+  create-installer ${INSTALLER_TYPE} \
+  --module-path ${MODULE_PATH} \
+  --verbose \
+  --echo-mode \
+  --add-modules java.base,java.datatransfer,java.desktop,java.scripting,java.xml,jdk.jsobject,jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,javafx.controls,javafx.fxml,java.naming,java.sql \
+  --input "${INPUT}" \
+  --output "${OUTPUT}" \
+  --name Santulator \
+  --main-jar ${JAR} \
+  --version ${VERSION} \
+  --jvm-args '--add-opens javafx.base/com.sun.javafx.reflect=ALL-UNNAMED' \
+  --class io.github.santulator.gui.main.SantulatorGuiExecutable
