f514ada2c8cce95009fee2bacace638419153cbd
Adam Carroll <AdamCarroll@users.noreply.github.com>
2018-10-29 10:40:23 +0100

    Windows packager batch file now calls java directly

--- package/bin/build-package.bat
+++ package/bin/build-package.bat
@@ -6,16 +6,20 @@ set OUTPUT=%5
 set JAR=%6
 set VERSION=%7
 
-call %PACKAGER% ^
-  create-image ^
-  --module-path %MODULE_PATH% ^
-  --verbose ^
-  --echo-mode ^
-  --add-modules java.base,java.datatransfer,java.desktop,java.scripting,java.xml,jdk.jsobject,jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,javafx.controls,javafx.fxml,java.naming,java.sql ^
-  --input "%INPUT%" ^
-  --output "%OUTPUT%" ^
-  --name Santulator ^
-  --main-jar %JAR% ^
-  --version %VERSION% ^
-  --jvm-args "--add-opens javafx.base/com.sun.javafx.reflect=ALL-UNNAMED" ^
-  --class io.github.santulator.gui.main.SantulatorGuiExecutable
+call "%JAVA_HOME%\bin\java.exe" ^
+    -Xmx512M ^
+    --module-path "%JAVA_HOME%\jmods" ^
+    --add-opens jdk.jlink/jdk.tools.jlink.internal.packager=jdk.packager ^
+    -m jdk.packager/jdk.packager.Main ^
+    create-image ^
+    --module-path "%MODULE_PATH%" ^
+    --verbose ^
+    --echo-mode ^
+    --add-modules "java.base,java.datatransfer,java.desktop,java.scripting,java.xml,jdk.jsobject,jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,javafx.controls,javafx.fxml,java.naming,java.sql" ^
+    --input "%INPUT%" ^
+    --output "%OUTPUT%" ^
+    --name "Santulator" ^
+    --main-jar "%JAR%" ^
+    --version "%VERSION%" ^
+    --jvm-args "--add-opens javafx.base/com.sun.javafx.reflect=ALL-UNNAMED" ^
+    --class "io.github.santulator.gui.main.SantulatorGuiExecutable"
