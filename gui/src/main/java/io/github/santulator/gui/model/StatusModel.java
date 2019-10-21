c406848d2555c60839f6f14fd64c3082586d7faa
Adam Carroll <AdamCarroll@users.noreply.github.com>
2019-10-21 22:01:51 +0200

    Improved handling of status transitions

--- gui/src/main/java/io/github/santulator/gui/model/StatusModel.java
+++ gui/src/main/java/io/github/santulator/gui/model/StatusModel.java
@@ -4,6 +4,7 @@
 
 package io.github.santulator.gui.model;
 
+import io.github.santulator.gui.i18n.I18nKey;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -18,15 +19,59 @@ public class StatusModel {
 
     private final SimpleBooleanProperty busy = new SimpleBooleanProperty();
 
+    private ActionStatus actionStatus = ActionStatus.MAIN;
+
+    private I18nKey secondaryAction;
+
+    public String getText() {
+        return text.get();
+    }
+
+    public void setText(final String text) {
+        this.text.set(text);
+    }
+
     public SimpleStringProperty textProperty() {
         return text;
+    }
+
+    public double getActivity() {
+        return activity.get();
+    }
+
+    public void setActivity(final double activity) {
+        this.activity.set(activity);
     }
 
     public SimpleDoubleProperty activityProperty() {
         return activity;
     }
 
+    public boolean isBusy() {
+        return busy.get();
+    }
+
+    public void setBusy(final boolean busy) {
+        this.busy.set(busy);
+    }
+
     public SimpleBooleanProperty busyProperty() {
         return busy;
+    }
+
+    public ActionStatus getActionStatus() {
+        return actionStatus;
+    }
+
+    public void setActionStatus(final ActionStatus actionStatus) {
+        this.actionStatus = actionStatus;
+    }
+
+    public I18nKey getSecondaryAction() {
+        return secondaryAction;
+    }
+
+    public void setSecondaryAction(final I18nKey secondaryAction) {
+        this.secondaryAction = secondaryAction;
     }
 }
--- gui/src/main/java/io/github/santulator/gui/status/StatusManagerImpl.java
+++ gui/src/main/java/io/github/santulator/gui/status/StatusManagerImpl.java
@@ -6,34 +6,30 @@ package io.github.santulator.gui.status;
 
 import io.github.santulator.gui.i18n.I18nKey;
 import io.github.santulator.gui.i18n.I18nManager;
+import io.github.santulator.gui.model.ActionStatus;
 import io.github.santulator.gui.model.StatusModel;
 import io.github.santulator.session.FileNameTool;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleStringProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.Optional;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 import static io.github.santulator.gui.i18n.I18nKey.*;
-import static javafx.beans.binding.Bindings.when;
 
 @Singleton
 public class StatusManagerImpl implements StatusManager {
     private static final Logger LOG = LoggerFactory.getLogger(StatusManagerImpl.class);
 
+    private static final int ACTIVITY_ON = -1;
+
+    private static final int ACTIVITY_OFF = 0;
+
     private final I18nManager i18nManager;
 
-    private I18nKey currentAction;
-
-    private final SimpleBooleanProperty busy = new SimpleBooleanProperty();
-
-    private final SimpleStringProperty actionDescription = new SimpleStringProperty();
-
-    private final AtomicBoolean gatekeeper = new AtomicBoolean();
+    private StatusModel model;
 
     @Inject
     public StatusManagerImpl(final I18nManager i18nManager) {
@@ -42,74 +38,121 @@ public class StatusManagerImpl implements StatusManager {
 
     @Inject
     public void setStatusModel(final StatusModel model) {
-        model.textProperty().bind(when(busy).then(actionDescription).otherwise(""));
-        model.busyProperty().bind(busy);
-        model.activityProperty().bind(when(busy).then(-1).otherwise(0));
+        this.model = model;
     }
 
     @Override
     public boolean beginNewSession() {
-        return begin(ACTION_NEW);
+        return beginSecondary(ACTION_NEW);
     }
 
     @Override
     public boolean beginOpenSession() {
-        return begin(ACTION_OPEN);
+        return beginSecondary(ACTION_OPEN);
     }
 
     @Override
     public boolean beginImportSession() {
-        return begin(ACTION_IMPORT);
+        return beginSecondary(ACTION_IMPORT);
     }
 
     @Override
     public boolean beginSaveSession() {
-        return begin(ACTION_SAVE);
+        return beginSecondary(ACTION_SAVE);
     }
 
     @Override
     public boolean beginRunDraw() {
-        return begin(ACTION_RUN);
-    }
-
-    @Override
-    public boolean beginExit() {
-        return begin(ACTION_EXIT);
+        return beginSecondary(ACTION_RUN);
     }
 
     @Override
     public boolean beginAbout() {
-        return begin(ACTION_ABOUT);
+        return beginSecondary(ACTION_ABOUT);
     }
 
-    private boolean begin(final I18nKey key) {
-        if (gatekeeper.compareAndSet(false, true)) {
-            currentAction = key;
-            LOG.debug("Begin: {}", currentAction);
-            actionDescription.setValue(i18nManager.text(key));
-            busy.setValue(true);
+    private boolean beginSecondary(final I18nKey key) {
+        Optional<ActionStatus> transition = model.getActionStatus().beginSecondary();
 
-            return true;
-        } else {
-            return false;
-        }
+        transition.ifPresent(s -> begin(key, s));
+
+        return transition.isPresent();
+    }
+
+    @Override
+    public boolean beginExit() {
+        Optional<ActionStatus> transition = model.getActionStatus().beginExit();
+
+        transition.ifPresent(s -> begin(ACTION_EXIT, s));
+
+        return transition.isPresent();
+    }
+
+    private void begin(final I18nKey key, final ActionStatus actionStatus) {
+        LOG.debug("Begin: {}", key);
+        transition(key, actionStatus);
     }
 
     @Override
     public void performAction(final Path file) {
-        LOG.debug("Perform: {}", currentAction);
-        actionDescription.setValue(String.format("%s: '%s'...", i18nManager.text(currentAction), FileNameTool.filename(file)));
+        I18nKey key = model.getSecondaryAction();
+
+        LOG.debug("Perform: {}", key);
+
+        model.setText(String.format("%s: '%s'...", i18nManager.text(key), FileNameTool.filename(file)));
     }
 
     @Override
     public void markSuccess() {
-        LOG.debug("Success: {}", currentAction);
+        LOG.debug("Success: {}", getActionKey());
     }
 
     @Override
     public void completeAction() {
-        LOG.debug("Complete: {}", currentAction);
-        busy.setValue(false);
-        gatekeeper.set(false);
+        ActionStatus newStatus = model.getActionStatus().complete()
+            .orElseThrow(() -> new IllegalStateException("No action to complete"));
+
+        LOG.debug("Complete: {}", getActionKey());
+        transition(model.getSecondaryAction(), newStatus);
+    }
+
+    private void transition(final I18nKey key, final ActionStatus status) {
+        boolean isBusy = status.isBusy();
+
+        model.setBusy(isBusy);
+        model.setActionStatus(status);
+        model.setText(text(key, status));
+        model.setActivity(activityValue(isBusy));
+        if (status == ActionStatus.SECONDARY) {
+            model.setSecondaryAction(key);
+        } else if (status == ActionStatus.MAIN) {
+            model.setSecondaryAction(null);
+        }
+    }
+
+    private double activityValue(final boolean isBusy) {
+        if (isBusy) {
+            return ACTIVITY_ON;
+        } else {
+            return ACTIVITY_OFF;
+        }
+    }
+
+    private String text(final I18nKey key, final ActionStatus status) {
+        if (status == ActionStatus.MAIN) {
+            return "";
+        } else if (status == ActionStatus.SECONDARY) {
+            return i18nManager.text(key);
+        } else {
+            return i18nManager.text(ACTION_EXIT);
+        }
+    }
+
+    private I18nKey getActionKey() {
+        if (model.getActionStatus() == ActionStatus.SECONDARY) {
+            return model.getSecondaryAction();
+        } else {
+            return ACTION_EXIT;
+        }
     }
 }
